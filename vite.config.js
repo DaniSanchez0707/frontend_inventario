4300a4e3591328fc1a6b6c8126b3fad589c9a0ff
DaniSanchez0707 <danisanc927@gmail.com>
2024-08-07 20:58:01 -0600

    Frontend Corriendo 2

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Permite conexiones desde fuera del contenedor
-    port: 80 // El puerto debe coincidir con el expuesto
-  }
-  
+  plugins: [react()]
+
 })
